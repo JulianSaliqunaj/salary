8e129d1395a8120c7374fcf2278829d412b81e9d
JulianSaliqunaj <juliansaliqunaj8@gmail.com>
2024-07-16 10:06:16 +0200

    Add files via upload

---
+++ index7.py
@@ -0,0 +1,7 @@
+salary = [340 ,210 ,450, 600, 230, 500, 550, 300]
+shumaEpagave = 0
+for y in salary:
+    shumaEpagave = shumaEpagave + y
+print(f"shuma e pagave eshte: {shumaEpagave}")
+pagamesatare = shumaEpagave / len(salary)
+print(f"paga mesatare eshte:{pagamesatare}")
